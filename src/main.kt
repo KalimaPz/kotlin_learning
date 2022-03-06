31cf9c095fd3a81f7e827bae86ab28fd74014248
oilkook <s6135512017@phuket.psu.ac.th>
2022-03-07 01:18:29 +0700

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var greeting : String = "Hello World!" // variable
+    val constant_number : Number = 1200 // immutable data
+
+    println(greeting)
+    println(constant_number)
+}
