188c524737ad2d769b90c38adf9426e408ecf50c
KalimaPz <kalima007c@gmail.com>
2022-03-07 01:21:38 +0700

    .

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,7 @@
 fun main() {
     var greeting : String = "Hello World!" // variable
-    val constant_number : Number = 1200 // immutable data
+    val constantNumber : Number = 1200 // immutable data
 
     println(greeting)
-    println(constant_number)
+    println(constantNumber)
 }
